5d39bcdef1b86fed63ffcef7d08e8683771347a4
Mark Maglana <mmaglana@gmail.com>
2020-01-28 16:36:57 +0200

    Initial commit

---
+++ src/charm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.6
+
+import sys
+sys.path.append('lib')
+
+from ops.charm import CharmBase
+from ops.main import main
+from ops.model import (
+    MaintenanceStatus,
+)
+
+
+class DemoCharm(CharmBase):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.start, self)
+
+    def on_start(self, event):
+        self.framework.model.unit.status = \
+            MaintenanceStatus('Configuring container')
+        return
+
+
+if __name__ == "__main__":
+    main(DemoCharm)
